92363d549a153c1924be04b74d55634066fc176b
geleeroyale <krassvs@gmail.com>
2023-04-12 20:05:41 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'giveth', // Usually your GitHub org/user name.
   projectName: 'givethai', // Usually your repo name.
-  trailingSlash: false,
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
