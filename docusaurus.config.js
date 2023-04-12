f262563196298fb457fecbafe3c7715f981d6248
geleeroyale <krassvs@gmail.com>
2023-04-12 18:38:24 +0200

    add trailingslash in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'giveth', // Usually your GitHub org/user name.
   projectName: 'givethai', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
